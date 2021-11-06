cceb8feb565a8d3ad6971584ebc76e29b5676d8a
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2021-11-06 16:04:08 +0100

    modified line 15, cleaned up and destructured action.payload

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -3,15 +3,16 @@ import {createSlice} from '@reduxjs/toolkit';
 export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
 const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
 
+console.log(initialState);
+
+
 const budgetsSlice = createSlice({
   name: 'budgets',
   initialState: initialState,
   reducers: {
     editBudget: (state, action) => {
       //extract amount and category
-      // const { category, amount } = action.payload;
-      const category = action.payload.category;
-      const amount = action.payload.amount;
+      const { category, amount } = action.payload;
       /*
       The find() method returns the value of the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
       */
